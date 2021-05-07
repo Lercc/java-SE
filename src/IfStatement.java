20fb89f1064de8bccdd2bc000d7bf166d25c9d83
lercc <lercc.en@gmail.com>
2021-05-06 23:17:25 -0500

    if statement

---
+++ src/IfStatement.java
@@ -0,0 +1,13 @@
+public class IfStatement {
+    public static void main (String[] args) {
+
+        boolean  isBlueetoorthEnabled = true;
+        int fileSended = 3;
+
+        // IF STATEMENT
+        if (isBlueetoorthEnabled) {
+            fileSended++;
+            System.out.println("3 Files sended ");
+        }
+    }
+}
